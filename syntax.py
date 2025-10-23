199e60f63466c76080b8f0417183ff272079cba3
Rajadurai <166482225+Rajadurai-M@users.noreply.github.com>
2025-10-23 21:45:43 +0530

    Added Python Syntax Practice File
    This file includes my practice code for Python syntax, output, comments, and variables, completed.

---
+++ syntax.py
@@ -0,0 +1,75 @@
+# Syntax
+# Indentation practice
+# for single space 
+# i=2
+"""
+if i<10:
+ print(i)
+"""
+# For four space 
+"""
+if i<3:
+    print("I is less than 3")
+"""
+
+"""
+if i<4:
+ print("i am raja")
+    print("raja")
+""" #Error message  Reason block of code same indentation not followed 
+
+# To print text using print function 
+# Statement 
+# single line print() statement 
+# print("Hi i am  rajadurai ")
+
+# Multiline statement 
+# print("Hi")
+# print("I am")
+# print("Rajadurai")
+
+# semicolon 
+# print("Hi") ;print("I am") ;print("Rajadurai")  
+
+"""
+end parameter :
+		In python end parameter used to print same line your text 
+"""
+# print("Hi",end=" ")
+# print("I am",end=" ") 
+# print("Rajadurai")
+
+# To print numbers 
+# print(5)
+# print(5+5)
+
+# Mix text and numbers
+
+# print("Hi i am rajadurai My age is",25,"I have",3,"Year experience in technical support advisor role") 
+
+# variables 
+
+"""
+x=5
+y="Rajadurai"
+i=True 
+print(x,y,i)
+"""
+# x=3.4
+# print(type(x))
+
+# Casting
+
+# x=str(3)
+# y=bool(3)
+# z=float(3)
+# print(type(x),type(y),type(z))
+
+# variables are casesensitives 
+
+# a=4
+# A=5
+# print(a,",",A)
+
+
+
